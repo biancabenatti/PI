72fa66b25d71382769eaac7786384cc17408c536
Bianca Benatti <bianca.benatti@bitize.com.br>
2024-10-22 16:18:54 -0300

    commit

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -1,39 +1,45 @@
 import React, { useState } from 'react';
 import './FAQ.css';
+import faq from '../../assets/FAQ.jpg';
+
 
 const FAQ = () => {
-    const [openIndex, setOpenIndex] = useState(null); // openIndex armazenará o índice da pergunta que está aberta. Quando o componente é montado, o openIndex é inicializado como null. Isso significa que nenhuma resposta está visível inicialmente.
-    // Quando o usuário clica no botão + ou - para uma pergunta, a função toggleAnswer é chamada, passando o índice da pergunta clicada
-    //Se o índice da pergunta que está sendo clicada é o mesmo que o openIndex atual, isso significa que a resposta já está aberta, então setOpenIndex é chamado com null, fechando a resposta.
-    //Se o índice é diferente, setOpenIndex armazena o novo índice, abrindo a resposta correspondente.
-   
-    const toggleAnswer = (index) => { // Função para alternar a visibilidade da resposta
-      setOpenIndex(openIndex === index ? null : index);
-    };
-  
-    const faqs = [
-      { question: "Pergunta 1", answer: "Essa é a resposta para a pergunta 1." },
-      { question: "Pergunta 2", answer: "Essa é a resposta para a pergunta 2." },
-      //  mais perguntas 
-    ];
-  
-    return (
-      <div>
-        <h2 id="faq-section">Perguntas Frequentes (FAQ)</h2>
+  const [openIndex, setOpenIndex] = useState(null); // openIndex armazenará o índice da pergunta que está aberta. Quando o componente é montado, o openIndex é inicializado como null. Isso significa que nenhuma resposta está visível inicialmente.
+  // Quando o usuário clica no botão + ou - para uma pergunta, a função toggleAnswer é chamada, passando o índice da pergunta clicada
+  //Se o índice da pergunta que está sendo clicada é o mesmo que o openIndex atual, isso significa que a resposta já está aberta, então setOpenIndex é chamado com null, fechando a resposta.
+  //Se o índice é diferente, setOpenIndex armazena o novo índice, abrindo a resposta correspondente.
+
+  const toggleAnswer = (index) => { // Função para alternar a visibilidade da resposta
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  const faqs = [
+    { question: "Como o sistema ajuda com a documentação de impostos?", answer: "O sistema organiza seus documentos relacionados aos impostos, facilitando a consulta e a apresentação de comprovantes quando necessário." },
+    { question: "O que devo fazer se encontrar um erro ao fazer upload?", answer: "Se você encontrar um erro, verifique sua conexão com a internet e o formato do arquivo. Se o problema persistir, entre em contato com nosso suporte técnico." },
+    { question: "Como posso entrar em contato com o suporte?", answer: "Você pode entrar em contato com nosso suporte enviando um e-mail para notagestsuporte@outlook.com" },
+    { question: "Como posso recuperar um arquivo que deletei?", answer: "Uma vez que um arquivo é deletado, não podemos recuperá-lo. Recomendamos ter cópias de segurança dos documentos mais importantes." },
+    { question: "Quais tipos de documentos posso armazenar relacionados à minha casa?", answer: "Você pode armazenar documentos como escrituras, contratos de compra e venda, comprovantes de pagamento de impostos, plantas baixas, e documentos de reformas." },
+  ];
+
+  return (
+    <div class="faq-container">
+      <img src={faq} alt="FAQ" class="faq-image" />
+      <div class="faq" id="faq-section">
+        <h2>Perguntas Frequentes (FAQ)</h2>
         {/* Mapeia sobre o array de perguntas e respostas */}
         {faqs.map((faq, index) => (
-          <div key={index}>
+          <div key={index} class="question-faq">
             <strong>{faq.question}</strong>
             {/* Botão que alterna a resposta ao ser clicado */}
-            <button onClick={() => toggleAnswer(index)}>
+            <button class="button" onClick={() => toggleAnswer(index)}>
               {openIndex === index ? '-' : '+'} {/* Exibe '-' se a pergunta estiver aberta, caso contrário exibe '+' */}
             </button>
-           
-            {openIndex === index && <div><p>{faq.answer}</p></div>}  {/* Renderiza a resposta apenas se o índice da pergunta atual for o mesmo que openIndex. Se a expressão à esquerda do && for verdadeira (ou seja, não for null, undefined, false, 0, ou uma string vazia), então a expressão à direita será avaliada e seu resultado será retornado.*/}
+            {openIndex === index && <div><p class="answer">{faq.answer}</p></div>}  {/* Renderiza a resposta apenas se o índice da pergunta atual for o mesmo que openIndex. Se a expressão à esquerda do && for verdadeira (ou seja, não for null, undefined, false, 0, ou uma string vazia), então a expressão à direita será avaliada e seu resultado será retornado.*/}
           </div>
         ))}
       </div>
-    );
+    </div>
+  );
 };
 
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Footer.css';
+import Logo from '../../assets/Logo.png';
 
 const Footer = () => {
     return (
         <footer class="footer">
             <div class="footer-content">
-                <img src="../../public/favicon.ico" alt="Logo" class="footer-logo" />
+                <img src={Logo} alt="Logo" class="footer-logo" />
+                <p>xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx</p>
                 <p>© 2024 Farj-Br. Todos os direitos reservados.</p>
             </div>
         </footer>
