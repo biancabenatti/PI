289c75985e0bb0760398e8800834c61c229ed65c
Bianca Benatti <bianca.benatti@bitize.com.br>
2024-10-25 14:58:00 -0300

    commit

--- src/components/Tips/Tips.js
+++ src/components/Tips/Tips.js
@@ -1,27 +1,37 @@
 import React, { useEffect } from 'react';
-import './Tips.css'; 
+import './Tips.css';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 const Tips = () => {
   const tipsData = [
     {
       title: "Construção de casas: documentação necessária",
       description: "Saiba quais são os documentos necessários para a construção de casas.",
-      link: "https://avt.com.br/construcao-de-casas-documentacao-necessaria/",  
+      link: "https://avt.com.br/construcao-de-casas-documentacao-necessaria/",
     },
     {
       title: "Dica 2",
       description: "Utilize cores diferentes para categorizar suas notas.",
-      link: "", 
+      link: "",
     },
     {
       title: "Dica 3",
       description: "Revise suas notas regularmente para melhor retenção.",
-      link: "", 
+      link: "",
     },
   ];
 
+  useEffect(() => {
+    AOS.init();
+  }, []);
+
+
   return (
-    <div className="tips-container" id="tips">
+    <div className="tips-container" id="tips"
+      data-aos="fade-up"
+      data-aos-anchor-placement="top-bottom"
+       data-aos-duration="1000">
       {tipsData.map((tip, index) => (
         <div key={index} className="tip-card" data-aos="fade-up">
           <h3>{tip.title}</h3>
